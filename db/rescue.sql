ef93007a68cf60a43df7265c0ca422f333ebef77
rahulr311295 <rahulraveendran06@gmail.com>
2018-08-16 23:14:28 +0530

    DB

--- db/rescue.sql
+++ db/rescue.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.20
--- https://www.phpmyadmin.net
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 16, 2018 at 02:00 PM
--- Server version: 5.5.54
--- PHP Version: 5.4.45
+-- Host: 127.0.0.1
+-- Generation Time: Aug 16, 2018 at 07:43 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `rescue`
@@ -26,17 +28,85 @@ SET time_zone = "+00:00";
 -- Table structure for table `markers`
 --
 
-CREATE TABLE IF NOT EXISTS `markers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `markers` (
+  `id` int(11) NOT NULL,
   `name` varchar(500) NOT NULL,
   `address` varchar(500) NOT NULL,
   `mobile` varchar(20) NOT NULL,
   `lat` float(16,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
   `type` varchar(30) DEFAULT 'Stranded',
-  `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rescue`
+--
+
+CREATE TABLE `rescue` (
+  `id` int(11) NOT NULL,
+  `mobile` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stranded`
+--
+
+CREATE TABLE `stranded` (
+  `id` int(11) NOT NULL,
+  `help_number` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `markers`
+--
+ALTER TABLE `markers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rescue`
+--
+ALTER TABLE `rescue`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `mobile` (`mobile`);
+
+--
+-- Indexes for table `stranded`
+--
+ALTER TABLE `stranded`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `help_number` (`help_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `markers`
+--
+ALTER TABLE `markers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `rescue`
+--
+ALTER TABLE `rescue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stranded`
+--
+ALTER TABLE `stranded`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
